d54d400d75724449fdd29d366692f2aa61b60abd
josea014 <53947223+josea014@users.noreply.github.com>
2022-04-26 12:28:38 -0500

    Add files via upload

---
+++ src/app/Componentes/Consultas/consultas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta',
+  templateUrl: './consultas.component.html',
+  styleUrls: ['./consultas.component.css']
+})
+export class ConsultaComponent {
+  title = 'GestionF-Consulta2022';
+}
