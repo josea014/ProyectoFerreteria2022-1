edcaa2d29ac65d6144cad2f2df1d66bcbfbbb4e8
josea014 <53947223+josea014@users.noreply.github.com>
2022-04-26 12:30:43 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { APP_ROUTING } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NaveComponent } from './Componentes/nave/nave.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { ContenidoComponent } from './Componentes/contenido/contenido.component';
+import { PerfilComponent } from './Componentes/perfil/perfil.component';
+import { ProductoComponent } from './Componentes/producto/producto.component';
+import { VentasComponent } from './Componentes/ventas/ventas.component';
+import { ClienteComponent } from './Componentes/cliente/cliente.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NaveComponent,
+    InicioComponent,
+    ContenidoComponent,
+    PerfilComponent,
+    ProductoComponent,
+    VentasComponent,
+    ClienteComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING,
+    MatSidenavModule,
+    MatSelectModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
