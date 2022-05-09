98d65847a706170a5a9a72745770c9919164d992
fdcaballero <55260529+fdcaballero@users.noreply.github.com>
2022-05-09 14:40:23 -0500

    Arreglo del Sidebar y obteniendo data de formularios ventas y perfil.

--- src/app/Componentes/ventas/ventas.component.ts
+++ src/app/Componentes/ventas/ventas.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,11 +10,29 @@ export class VentasComponent implements OnInit {
 
   disabled: boolean;
   cantidades: number[];
-  constructor() {
+  form;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      empresa:'',
+      nombreProducto:'',
+      factura:'',
+      datoCliente: '',
+      descripcion: '',
+      cantidad: '',
+      codigoBarra: '',
+      fecha:'',
+      descuento: '',
+      precio: '',
+      total: '',
+    })
     this.disabled = true;
-    this.cantidades = [1,2,3,4,5,8,4,6,6,9,10,12]
+    this.cantidades = [1, 2, 3, 4, 5, 8, 4, 6, 6, 9, 10, 12]
   }
   ngOnInit(): void {
+  }
+  onSubmit(data:object):void {
+    console.log(data);
+    this.form.reset();
   }
 
 }
